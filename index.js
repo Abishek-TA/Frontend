246b46b1be7423a4572cf1625143d047ae5a5300
Arrow_Grub <abtiriyan19@gmail.com>
2023-12-09 16:46:22 +0530

    add responsive action to slider

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const prev = document.getElementById('prev-btn')
 const next = document.getElementById('next-btn')
 const list = document.getElementById('item-list')
 
-const itemWidth = 150
-const padding = 10
+const itemWidth = 200
+const padding = 40
 
 prev.addEventListener('click',()=>{
   list.scrollLeft -= itemWidth + padding
