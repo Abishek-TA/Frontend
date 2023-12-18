fe08afce5af2dd2a8e48a6252d6f483bfe812db3
Arrow_Grub <abtiriyan19@gmail.com>
2023-12-18 20:00:35 +0530

    header separated

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -1,3 +1,4 @@
+$('#head1').load('Header1.html')
 const prev = document.getElementById('prev-btn')
 const next = document.getElementById('next-btn')
 const list = document.getElementById('item-list')
