0b0bf70cc2c76bbe5324ba928bdd11d76d43142f
Arrow_Grub <abtiriyan19@gmail.com>
2023-12-19 19:57:59 +0530

    add js to form

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -1,11 +1,10 @@
-$('#head1').load('Header1.html')
+$('#head1').load('Header1.html')/*access the header*/
+
 const prev = document.getElementById('prev-btn')
 const next = document.getElementById('next-btn')
 const list = document.getElementById('item-list')
-
 const itemWidth = 200
 const padding = 100
-
 prev.addEventListener('click',()=>{
   list.scrollLeft -= itemWidth + padding
 })
