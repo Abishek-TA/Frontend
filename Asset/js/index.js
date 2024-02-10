0faca9952de3abebbe9b765f006af24324afaa3e
Sajan-kumar007 <153884098+Sajan-kumar007@users.noreply.github.com>
2024-02-10 12:44:43 +0530

    fix bug

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -1,15 +1,15 @@
-$("#head1").load("Header1.html"); /*access the header*/
-$(".footer").load("Footer.html"); /*access the header*/
-$(".sider").load("sider1.html"); /*access the sider*/
-const prev = document.getElementById("prev-btn");
-const next = document.getElementById("next-btn");
-const list = document.getElementById("item-list");
-const itemWidth = 200;
-const padding = 100;
-prev.addEventListener("click", () => {
-  list.scrollLeft -= itemWidth + padding;
-});
+$('#head1').load('Header1.html')/*access the header*/
+$('.footer').load('Footer.html')/*access the header*/
+$('.sider').load('sider1.html')/*access the sider*/
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+const itemWidth = 200
+const padding = 90
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= itemWidth + padding
+})
 
-next.addEventListener("click", () => {
-  list.scrollLeft += itemWidth + padding;
-});
+next.addEventListener('click',()=>{
+  list.scrollLeft += itemWidth + padding
+})
