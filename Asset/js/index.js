df45d574fe10338c5cb50a058c4c90cd633782d0
Arrow_Grub <abtiriyan19@gmail.com>
2024-02-29 19:11:36 +0530

    button css design

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -1,6 +1,7 @@
 $('#head1').load('Header1.html')/*access the header*/
 $('.footer').load('Footer.html')/*access the header*/
 $('.sider').load('sider1.html')/*access the sider*/
+$('.sbar').load('search.html')/*access the search bar*/
 const prev = document.getElementById('prev-btn')
 const next = document.getElementById('next-btn')
 const list = document.getElementById('item-list')
